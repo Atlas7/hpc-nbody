e9856b73ddd23d870838501b6bbdf639b46057e7
Johnny Chan <johnnychan0302@gmail.com>
2017-09-05 03:27:06 -0700

    correct imp 9

--- imp-09/nbody.cc
+++ imp-09/nbody.cc
@@ -58,10 +58,7 @@ void MoveParticles(const int nParticles, ParticleSetType & particle, const float
   }
 
 
-  // Move particles according to their velocities
-  // O(N) work, so using a serial loop
-#pragma simd
-#pragma vector aligned
+#pragma omp parallel for
   for (int i = 0 ; i < nParticles; i++) { 
     particle.x[i]  += particle.vx[i]*dt;
     particle.y[i]  += particle.vy[i]*dt;
